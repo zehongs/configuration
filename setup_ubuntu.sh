5c91259f99722eca8e0a8522013d9c745a5f1e0a
shenzehong <zhshen0917@gmail.com>
2020-05-09 12:14:48 +0800

    ADD: setup ubuntu with omzsh,tmux,locales(with dc-file)

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+sudo apt-get update
+
 ####################
-# zsh
+# zsh, locales(fix)
 ####################
-apt-get -y install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+apt-get -y install zsh locales
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /bin/zsh
-wget "https://raw.githubusercontent.com/zehongs/configuration/master/cfg/.zshrc"
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git)/plugins=(zsh-autosuggestions git zsh-syntax-highlighting)/g' ~/.zshrc
+
+locale-gen "en_US.UTF-8" && update-locale LC_ALL="en_US.UTF-8"
+
 
 ####################
-# tmux
+# tmux > 2.6
 ####################
-sudo apt-get install tmux
+apt-get install -y tmux
 touch ~/.tmux.conf
-echo "set-option -g mouse on
-
-# make scrolling with wheels work
-bind -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M' 'select-pane -t=; copy-mode -e; send-keys -M'"
-bind -n WheelDownPane select-pane -t= \; send-keys -M
-" > .tmux.conf
+echo "set -g mouse on" >> ~/.tmux.conf
 
 # website: tmuxcheatsheet.com
